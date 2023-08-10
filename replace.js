3a93bcf4ddb1d769b2a8a3b5c9a0a9c87409b60c
mehedi-h <mehedihs2015@gmail.com>
2023-08-10 13:51:11 +0600

    update search.js file

---
+++ replace.js
@@ -0,0 +1,13 @@
+
+const fruit = ['Mango', 'Apple', 'Banana', 'Mango', 'Jackfruit', 'Pineapple', 'Apple', 'Banana', 'Strawberry']
+
+function replaceFruit (fruit) {
+    const original = [];
+    for (let i = 0; < fruit.length; i++) {
+        const element = arr];
+        
+    }
+    if (original.includes(fruit)) {
+        
+    }
+}
--- search.js
+++ search.js
@@ -1,5 +1,33 @@
 
-const songs = "Amar Sonar Bangla Ami Tomay Valobashi";
 
-const search = songs.includes('Valobashi')
-console.log(search)
+// const songs = "Amar Sonar Bangla Ami Tomay Valobashi";
+
+// const search = songs.includes('Valobashi')
+// console.log(search)
+
+// const songs = ['Ami', 'mi']
+
+// if (songs[0] == songs[1]) {
+//     console.log('true');
+// } else {
+//     console.log('false')
+// }
+// console.log(search)
+
+
+// function matchFinder(string1, string2) {
+//     const search = matchFinder.includes()
+// }
+// matchFinder('hello', 'llo');
+
+function compareSubstring(string1, start1, end1, string2) {
+    const substring1 = string1.slice(start1, end1);
+    return substring1 === string2;
+  }
+  
+  const string1 = "abcdefgh";
+  const string2 = "bcd";
+  
+  const result = compareSubstring(string1, 1, 4, string2);
+  console.log(result);  // This will output: true
+  
